4a1a039d197b0dfe37e1873a58e5e270240bf944
Andrei <101584932+DashiellVienna@users.noreply.github.com>
2024-12-16 14:54:26 +0100

    Add files via upload
    tryinf to fix connection between Xcode and Github

---
+++ Lesson2/Lesson2Tests/Lesson2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson2Tests.swift
+//  Lesson2Tests
+//
+//  Created by Andrei Deshin on 06.11.24.
+//
+
+import Testing
+@testable import Lesson2
+
+struct Lesson2Tests {
+
+    @Test func example() async throws {
+        
+    }
+
+}
